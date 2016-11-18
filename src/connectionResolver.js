9035bf29757266fdc78f3c9f4d5da1606ee65bc8
Yoad Snapir <yoadsn@users.noreply.github.com>
2016-11-18 12:45:00 +0200

    Only call findMany when it's required When only `count` field is requested - only call the count resolver

--- src/connectionResolver.js
+++ src/connectionResolver.js
@@ -86,6 +86,7 @@ export function prepareConnectionResolver(
     },
     resolve: async (resolveParams: ConnectionResolveParams) => {
       let countPromise;
+      let findManyPromise;
       const { projection = {}, args, rawQuery } = resolveParams;
       const findManyParams: ResolveParams = Object.assign(
         {},
@@ -117,6 +118,13 @@ export function prepareConnectionResolver(
         countPromise = Promise.resolve(0);
       }
 
+      let onlyCountProjected = projection.count;
+      for (var property in projection) {
+          if (projection.hasOwnProperty(property) && property !== 'count') {
+            onlyCountProjected = false;
+            break;
+          }
+      }
 
       if (projection && projection.edges) {
         // combine top level projection (maybe somebody add additional fields via resolveParams.projection)
@@ -173,7 +181,15 @@ export function prepareConnectionResolver(
       resolveParams.findManyResolveParams = findManyParams;
       resolveParams.countResolveParams = countParams;
 
-      return Promise.all([findManyResolve(findManyParams), countPromise])
+      // This allows to optimize and not actually call the findMany resolver
+      // if only the count is required
+      if (onlyCountProjected) {
+        findManyPromise = Promise.resolve([]);
+      } else {
+        findManyPromise = findManyResolve(findManyParams);
+      }
+
+      return Promise.all([findManyPromise, countPromise])
         .then(([recordList, count]) => {
           const edges = [];
           // transform record to object { cursor, node }
